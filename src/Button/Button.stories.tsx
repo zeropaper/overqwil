80cc1978eb567a50102f270bdcf29739bbbeea92
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-18 14:37:47 +0200

    feat(Button): add base component

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { withGlobalStyles } from '../../.storybook/decorators';
+import { withGlobalStyles } from '../../.storybook/utils';
 
-import Button from './Button';
+import { Button } from './Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Overqwil/Button',
   component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    type: 'button',
+  },
+  argTypes: {
+    type: {
+      table: {
+        disable: true,
+      },
+    },
+  },
   decorators: [withGlobalStyles],
-  args: {},
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
   children: 'Button',
-  color: 'secondary',
+  decoSymbol: '#',
+  decoCode: 's01',
 };
