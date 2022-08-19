3a47c7ac4bf81c2ace10dd42c16a41118d9e3c61
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-19 18:52:40 +0200

    feat(Title): work on deco styles

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { Title as MTitle, TitleProps as MTitleProps } from '@mantine/core';
+import {
+  keyframes,
+  Title as MTitle,
+  TitleProps as MTitleProps,
+} from '@mantine/core';
 
 export interface PropTypes extends MTitleProps {
   noDecoration?: boolean;
@@ -9,36 +13,67 @@ export function Title({ noDecoration, ...props }: PropTypes) {
   return (
     <MTitle
       {...props}
-      sx={({ spacing, colors, fn, other, colorScheme }) =>
-        noDecoration
-          ? {}
-          : {
-              // tweaking maxWidth (instead of margin) to ensure
-              // that the "margin props" from Mantine are kept working
-              // marginRight: `calc(1ch + ${spacing.xs}px)`,
-              maxWidth: `calc(100% - (1ch + ${spacing.xs}px))`,
-              position: 'relative',
-              '&::before,&::after': {
-                position: 'absolute',
-                content: '""',
-                bottom: 0,
-                width: '100%',
-              },
-              '&::before': {
-                zIndex: -1,
-                left: '1ch',
-                backgroundColor: fn.primaryColor(),
-                height: spacing.xs * 0.5,
-              },
-              '&::after': {
-                left: `calc(1ch + ${spacing.xs}px)`,
-                backgroundColor: other.effectColor(colors, colorScheme),
-                zIndex: -2,
-                height: spacing.xs * 0.25,
-                bottom: spacing.xs * 0.25,
-              },
-            }
-      }
+      sx={({ spacing, colors, fn, other, colorScheme }) => {
+        if (noDecoration) return {};
+        const glitch = keyframes({
+          '0%': {
+            opacity: '0',
+          },
+          '49%': {
+            opacity: '0',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '51%': {
+            width: '12px',
+          },
+          '54%': {
+            width: '45%',
+            marginLeft: '10%',
+          },
+          '64%': {
+            width: '45%',
+            marginLeft: '10%',
+          },
+          '65%': {
+            opacity: '1',
+          },
+          '87%': {
+            opacity: '1',
+          },
+          '88%': {
+            opacity: '0',
+          },
+        });
+        return {
+          // tweaking maxWidth (instead of margin) to ensure
+          // that the "margin props" from Mantine are kept working
+          // marginRight: `calc(1ch + ${spacing.xs}px)`,
+          maxWidth: `calc(100% - (1ch + ${spacing.xs}px))`,
+          position: 'relative',
+          '&::before,&::after': {
+            position: 'absolute',
+            content: '""',
+            bottom: 0,
+            width: '100%',
+          },
+          '&::before': {
+            zIndex: -1,
+            left: '1ch',
+            backgroundColor: fn.primaryColor(),
+            height: spacing.xs * 0.5,
+            animation: `${glitch} ${Math.random() * 10}s infinite alternate`,
+          },
+          '&::after': {
+            left: `calc(1ch + ${spacing.xs}px)`,
+            backgroundColor: other.effectColor(colors, colorScheme),
+            zIndex: -2,
+            height: spacing.xs * 0.25,
+            bottom: spacing.xs * 0.25,
+          },
+        };
+      }}
     />
   );
 }
