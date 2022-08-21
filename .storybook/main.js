bee8b1b79a3a955b52d54fccd2b37cad9a94ea38
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-21 12:16:20 +0200

    build(sb): add actions plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,7 @@ module.exports = {
     // error @ index.js:56
     // that makes storybook unusable
     // '@storybook/addon-interactions',
+    '@storybook/addon-actions',
   ],
   framework: '@storybook/react',
   core: {
