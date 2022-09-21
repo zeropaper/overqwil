cbbe69526ae1ecf8aec77efca0a7c9992cb951e4
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-09-21 08:46:06 +0200

    refactor: remove leftovers

--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,8 @@ export type { PropTypes as OverqwilProviderProps } from './OverqwilProvider/Over
 export { default as Global } from './Global/Global';
 export type { PropTypes as GlobalProps } from './Global/Global';
 
-export { default as AppShell } from './AppShell/AppShell';
-export type { PropTypes as AppShellProps } from './AppShell/AppShell';
-
-export { default as Button } from './Button/Button';
-export type { PropTypes as ButtonProps } from './Button/Button';
-
-export { default as Title } from './Title/Title';
-export type { PropTypes as TitleProps } from './Title/Title';
-
 export { default as OverqwilLogo } from './OverqwilLogo/OverqwilLogo';
 export type { PropTypes as OverqwilLogoProps } from './OverqwilLogo/OverqwilLogo';
-
-export { default as Transition } from './Transition/Transition';
-export type { PropTypes as TransitionProps } from './Transition/Transition';
-
-export { default as TextInput } from './TextInput/TextInput';
-export type { PropTypes as TextInputProps } from './TextInput/TextInput';
-
-export { default as Input } from './Input/Input';
-export type { PropTypes as InputProps } from './Input/Input';
-
-export { default as Textarea } from './Textarea/Textarea';
-export type { PropTypes as TextareaProps } from './Textarea/Textarea';
 
 export { default as TextWriter } from './TextWriter/TextWriter';
 export type { PropTypes as TextWriterProps } from './TextWriter/TextWriter';
