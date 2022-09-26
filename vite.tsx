0ffc42f1e24d3b4301a43fc088e4688da33ca689
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-09-26 06:05:35 +0200

    chore: work on intro
    - add errorElement to routes

--- vite.tsx
+++ vite.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable spaced-comment, import/no-unresolved, import/no-extraneous-dependencies */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useRouteError,
+} from 'react-router-dom';
+import { Prism } from '@mantine/prism';
 
 import * as Overqwil from '.';
 
@@ -11,7 +16,13 @@ import withDemo from './demos/withDemo';
 import { demos } from './demos/index';
 import { Layout } from './demos/Layout';
 
-const { Box, Text, TextWriter, Title } = Overqwil;
+const {
+  Box,
+  Text,
+  // TextWriter,
+  Title,
+  Anchor,
+} = Overqwil;
 
 function Intro() {
   return (
@@ -23,23 +34,77 @@ function Intro() {
       }}
     >
       <Title pb="xl" mb="lg">
-        Overqwil UI
+        A futuristic take on the Mantine UI library.
       </Title>
 
+      {/*
       <TextWriter size="lg" mb="lg">
         I was browsing the Internet when I saw a truly amazing futuristic button
         design and decided to take it to the next level.
       </TextWriter>
+      */}
 
       <Text size="lg">
         This project is aimed at providing a futuristic theming system based on{' '}
-        <a href="https://mantine.dev">Mantine UI</a> components for React.
+        <Anchor href="https://mantine.dev">Mantine UI</Anchor> components for
+        React.
       </Text>
 
       <Text size="lg">
         It is also aimed as an example on how to customize a UI system like
-        Mantine UI or Material UI (since they are similar) with a company
-        branding.
+        Mantine UI or <Anchor href="https://mui.com/">Material UI</Anchor>{' '}
+        (since they are similar) with a company branding.
+        <br />
+        The project is still in its early stages, but you can already see it in
+        action.
+      </Text>
+
+      <Title order={2} mt="xl" mb="lg">
+        Usage
+      </Title>
+      <Text size="lg">
+        Overqwil is aimed at being used the same way as Mantine UI, but with a
+        different theme.
+      </Text>
+
+      <Title order={3} mt="xl" mb="lg">
+        Add the provider
+      </Title>
+      <Prism language="tsx">
+        {`
+import { OverqwilProvider } from 'overqwil';
+
+function App() {
+  return (
+    <OverqwilProvider theme={/* optional customization */}>
+      {/* Your app content */}
+    </OverqwilProvider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+        `}
+      </Prism>
+
+      <Title order={3} mt="xl" mb="lg">
+        Use the components
+      </Title>
+      <Prism language="tsx">
+        {`
+import { Button } from 'overqwil';
+
+function App() {
+  return <Button>Click me</Button>;
+}
+        `}
+      </Prism>
+
+      <Title order={2} mt="xl" mb="lg">
+        Technique used
+      </Title>
+      <Text size="lg">
+        In order to customize the theme is a combination of SVG images
+        generation, style overrides and CSS variables.
       </Text>
     </Box>
   );
@@ -49,7 +114,7 @@ function WithApp({
   component: Component,
   ...props
 }: {
-  component: React.ComponentType;
+  component: React.FunctionComponent<any>;
 }) {
   return (
     <AppContextProvider>
@@ -60,14 +125,25 @@ function WithApp({
   );
 }
 
+function ErrorBoundary() {
+  const error = useRouteError();
+  // eslint-disable-next-line no-console
+  console.error(error);
+  return <div>Dang!</div>;
+}
+
+const errorElement = <WithApp component={ErrorBoundary} />;
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <WithApp component={Intro} />,
+    errorElement,
   },
   ...Object.entries(demos).map(([name, Component]) => ({
     path: `/${name.toLowerCase()}`,
     element: <WithApp component={withDemo(name, Component)} />,
+    errorElement,
   })),
 ]);
 
