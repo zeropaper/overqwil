fd0eaaa7917c034bdf9125cffeeeed80679fc86b
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-09-27 10:33:32 +0200

    build: tweak vite config to reduce chunk sizes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,29 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+          'mantine-hooks': ['@mantine/hooks'],
+          'mantine-prism': ['@mantine/prism'],
+          'mantine-core': ['@mantine/core'],
+
+          'mantine-core-deps': [
+            '@tabler/icons',
+            '@mantine/utils',
+            'react-textarea-autosize',
+          ],
+          'mantine-styles': ['@mantine/styles'],
+          'radix-ui-react-scroll-area': ['@radix-ui/react-scroll-area'],
+          'floating-ui-react-dom-interactions': [
+            '@floating-ui/react-dom-interactions',
+          ],
+
+          overqwil: ['./src'],
+        },
+      },
+    },
+  },
 });
